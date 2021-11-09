cb4a4b49a94bf859a058e7e8f417bcf1a5333b3e
Fred Strauss <fred@strider.co.za>
2021-11-09 18:27:17 +0200

    Show provinces

--- app/Main.hs
+++ app/Main.hs
@@ -2,12 +2,30 @@ module Main where
 
 import Loadshedding
 
+provinces = [ "Eastern Cape"
+            , "Free State"
+            , "Gauteng"
+            , "KwaZulu-Natal"
+            , "Limpopo"
+            , "Mpumalanga"
+            , "Norh West"
+            , "Northern Cape"
+            , "Western Cape"
+            ]
+
+showProvinces :: IO ()
+showProvinces = mapM_ print' $ zip [1..] provinces
+  where print' (i,n) = putStrLn $ show i <> " " <> n
+
 main :: IO ()
 main = do
   client <- newLoadsheddingClient
   status <- getLoadsheddingStatus client
 
+  showProvinces
+  putStrLn ""
+
   case status of
     Left err  -> print err
-    Right 0   -> putStrLn "Not load shedding at the moment"
-    Right l   -> putStrLn $ "Load shedding level " <> show l
+    Right 0   -> putStrLn "Not load shedding"
+    Right l   -> putStrLn $ "Stage " <> show l
