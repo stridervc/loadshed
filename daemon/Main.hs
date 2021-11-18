0f78818d2ab63cbc3c1e179de7461fe66b1d5cb5
Fred Strauss <fred@strider.co.za>
2021-11-18 17:12:04 +0200

    Get seconds to next power outage

--- daemon/Main.hs
+++ daemon/Main.hs
@@ -7,8 +7,8 @@ import Data.Time.Format
 import Data.Time.Calendar
 import Data.Time.LocalTime
 
-nextShutdown :: DaySchedule -> IO ()
-nextShutdown (day, times) = do
+nextShutdownT :: DaySchedule -> IO ()
+nextShutdownT (day, times) = do
   -- Thu, 18 Nov
   -- 10:00
   now <- getCurrentTime
@@ -19,6 +19,23 @@ nextShutdown (day, times) = do
   print now
   print parsed
   print $ diffUTCTime parsed now
+
+-- change a DaySchedule to just be a list of strings of day and start time
+-- we add the year and timezone as well
+-- for example : "2021 Thu, 18 Nov 10:00 +0200"
+simplifyDaySchedule :: Integer -> DaySchedule -> [String]
+simplifyDaySchedule _ (_,[]) = []
+simplifyDaySchedule year (day, tr:trs) = simplified : simplifyDaySchedule year (day, trs)
+  where simplified  = show year <> " " <> day <> " " <> fst tr <> " +0200"
+
+nextShutdown :: UTCTime -> [DaySchedule] -> IO NominalDiffTime
+nextShutdown now schedules = do
+  let (year, _, _) = toGregorian $ utctDay now
+  let times = concatMap (simplifyDaySchedule year) schedules
+  times' <- mapM parse' times
+  return $ head $ filter (> 0) $ map (`diffUTCTime` now) times'
+  where parse'  :: String -> IO UTCTime
+        parse'  = parseTimeM False defaultTimeLocale "%Y %a, %d %b %R %z"
 
 main = do
   config <- loadConfig
@@ -38,4 +55,6 @@ main = do
       case schedule of
         Left err    -> putStrLn "Error getting schedule"
         Right sched -> do
-          nextShutdown $ head sched
+          now <- getCurrentTime
+          next <- nextShutdown now sched
+          print next
